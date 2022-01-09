cc56e7ca1f7b1c32634a7a699c920454865b2c0a
Matan Ben Moshe <matanbe18@gmail.com>
2022-01-09 11:39:51 +0200

    added color picker

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let isUpsideDown = false;
 
 setInterval(function () {
   update();
-}, 1000/60);
+}, 1000 / 60);
 
 function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -20,13 +20,34 @@ function update() {
   if (x <= 0) isReverse = false;
   x = isReverse ? x - 1.1 : x + 1.1;
 
-  if (y + 20 > canvas.height ) isUpsideDown = true;
+  if (y + 20 > canvas.height) isUpsideDown = true;
   if (y <= 0) isUpsideDown = false;
 
   y = isUpsideDown ? y - 1 : y + 1;
-  
 }
 
 function drawRect(x, y) {
   ctx.fillRect(x, y, 20, 20);
 }
+
+const colorArr = [
+  "#456c83",
+  "#714583",
+  "#45835e",
+  "#8f7247",
+  "#8f4747",
+  "#47508f",
+  "#478f85",
+  "#636469",
+];
+let nextIndex = 0;
+const button = document.getElementById("colorizer");
+button.addEventListener("click", function () {
+  document.getElementById("bouncer").style.backgroundColor =
+    colorArr[nextIndex];
+  button.style.backgroundColor =
+    colorArr[nextIndex + 1 === colorArr.length ? 0 : nextIndex + 1];
+  ctx.fillStyle = colorArr[nextIndex];
+  if (nextIndex === colorArr.length - 1) nextIndex = 0;
+  else nextIndex++;
+});
